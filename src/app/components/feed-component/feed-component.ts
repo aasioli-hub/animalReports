3fae8f8e663a008a3a820c89e81a6ddf93c0d8a6
aasioli-hub <a.asioli@accademiadigitaleliguria.it>
2025-10-24 15:40:01 +0200

    ordinamento per distanza

--- src/app/components/feed-component/feed-component.ts
+++ src/app/components/feed-component/feed-component.ts
@@ -2,24 +2,47 @@ import { Component, inject } from '@angular/core';
 import { DataService } from '../../services/data-service/data-service';
 import { Report } from '../../model/report';
 import { ReportCardComponent } from '../report-card-component/report-card-component';
+import { LocationService } from '../../services/location-service/location-service';
 
 @Component({
   selector: 'app-feed-component',
   imports: [ReportCardComponent],
   templateUrl: './feed-component.html',
-  styleUrl: './feed-component.scss'
+  styleUrl: './feed-component.scss',
 })
 export class FeedComponent {
-
   private dataServ = inject(DataService);
+  private locationServ = inject(LocationService);
   public reports: Report[] = [];
 
   constructor() {
-    this.dataServ.getReports()
-    .then((data) => {console.log(data); return data})
-    .then((data) => this.reports = data);
+    // this.dataServ.getReports().then((data) => {
+    //   if ('geolocation' in navigator) {
+    //     this.locationServ.sortReportsByDistance(data)
+    //     .then((sortedReports) => this.reports = sortedReports)
+    //   } else {
+    //     this.reports = data;
+    //   }
+    // });
+
+    // this.dataServ.getReports().then((data) => {
+    //   if ('geolocation' in navigator) {
+    //     return this.locationServ.sortReportsByDistance(data)
+    //   } else {
+    //     return data;
+    //   }
+    // }).then(newReports => this.reports = newReports);
+    this.loadReports()
   }
 
+  async loadReports(){
+    const unorderedReports = await this.dataServ.getReports()
+    if ('geolocation' in navigator) {
+        const orderdReports = await this.locationServ.sortReportsByDistance(unorderedReports);
+        this.reports = orderdReports;
+    } else {
+        this.reports = unorderedReports;
+    }
 
-
+  }
 }
